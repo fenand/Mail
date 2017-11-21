9f3b992d770185b4280299e77a0f39f95ec2b733
Fernando Garcia De Castro <garciaf4c@gmail.com>
2017-11-21 17:34:03 +0100

    Commit 9

--- Test.java
+++ Test.java
@@ -1,9 +1,6 @@
 
 public class Test
 {
-
-       
-        
     /**
      * Constructor for objects of class Test
      */
@@ -11,21 +8,73 @@ public class Test
     {
     }
 
-    
-    public void funcionalidad3()
+    public void funcionalidad03()
     {
-        System.out.println("Se va a crear un server con nombre server " + " Y dos usuarios con nombre 1 y 2");
+       
+
+        MailServer servidor = new MailServer(); 
+        String usuario1 = "fernando1";
+        MailClient cliente1 = new MailClient(servidor, usuario1);
+
+        String usuario2 = "fernando2";
+        MailClient cliente2 = new MailClient(servidor, usuario2);
+
+        String asunto = "Saludo";
+        String mensaje = "Hola";
+
         
-        MailServer server = new MailServer();
+        System.out.println("Creación de un servidor: " + servidor);
+        System.out.println("Creación de un cliente: " + cliente1);
+        System.out.println(cliente1 + " debería tener 0 mensajes enviados y recibidos al crearlo");
         
-        MailClient mailClie1 = new MailClient(server, "1");
+
         
-        MailClient mailClie2 = new MailClient(server, "2");
+        if(servidor.getNextMailItem(usuario1) == null){
+            System.out.println("¡Funciona perfectamente!");
+        }
+        else{
+            System.out.println("¡ERROR!");
+        }
+
         
+        System.out.println("Creación de un cliente: " + cliente2);
+        System.out.println(cliente2 + " debería tener 0 mensajes enviados y recibidos al crearlo");
+
+        if(servidor.getNextMailItem(usuario2) == null){
+            System.out.println("¡Funciona perfectamente!");
+        }
+        else{
+            System.out.println("¡ERROR!");
+        }
+
         
+        System.out.println(cliente1 + " envía un mensaje a " + cliente2);
+        System.out.println(cliente2 + " debería tener 1 mensaje recibido");
+
+        cliente1.sendMailItem(usuario2, asunto, mensaje);
+
+        if(servidor.getNextMailItem(usuario2) == null){
+            System.out.println("¡ERROR!");
+        }
+        else {
+            System.out.println("¡Funciona perfectamente, 1 mensaje recibido!");
+        }
+
         
-        
-        
-        
+        String re = "Re: "+ asunto;
+        String gracias = "Gracias por tu mensaje ya me ha llegado.\n " + "Tu Mensaje es: " +mensaje;
+        System.out.println(cliente2 + " reenvía el mensaje a " + cliente1);
+        System.out.println(cliente1 + " debería tener 1 mensaje recibido");
+
+        cliente2.sendMailItem(usuario1, re, gracias);
+
+        if(servidor.getNextMailItem(usuario1) == null){
+
+            System.out.println("¡ERROR!");
+        }
+        else {
+            System.out.println("¡Funciona perfectamente, 1 mensaje recibido!");
+            System.out.println( "cliente2 ha reenviado el mensaje "+ gracias + " con el asunto "+ re);
+        }
     }
 }
